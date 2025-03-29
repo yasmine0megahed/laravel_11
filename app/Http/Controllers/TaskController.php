e77dd6cbc9740872b486aa27a14584a7d66466d2
yasmine0megahed <yasmine.aboulmaaty.megahed@gmail.com>
2025-03-29 09:12:20 +0200

    added validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $tasks = Task::all();
         return response()->json(
             $tasks,
@@ -16,18 +17,25 @@ class TaskController extends Controller
     }
     public function store(Request $request)
     {
-
-        $task = Task::create([
-            'title' => $request->title,
-            'description' => $request->description,
-            'priority' => $request->priority
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:40|min:5',
+            'description' => 'required|string|min:10|max:255',
+            'priority' => 'required|integer|min:1|max:5'
         ]);
+        $task = Task::create($validatedData);
+
+        // $task = Task::create([
+        //     'title' => $request->title,
+        //     'description' => $request->description,
+        //     'priority' => $request->priority
+        // ]);
         return response()->json(
             $task,
             201
         );
     }
-    public function show($id){
+    public function show($id)
+    {
         $task = Task::findOrFail($id);
         return response()->json(
             $task,
@@ -37,15 +45,22 @@ class TaskController extends Controller
     public function update(Request $request, $id)
     {
         $task = Task::findOrFail($id);
-        $task->update(
-            $request->only('title', 'description', 'priority')
-        );
+        $validatedData = $request->validate([
+            'title' => 'sometimes|string|max:40|min:5',
+            'description' => 'sometimes|string|min:10|max:255',
+            'priority' => 'sometimes|integer|min:1|max:5'
+        ]);
+        $task->update($validatedData);
+        // $task->update(
+        //     $request->only('title', 'description', 'priority')
+        // );
         return response()->json(
             $task,
             200
         );
     }
-    public function destroy($id){
+    public function destroy($id)
+    {
         $task = Task::findOrFail($id);
         $task->delete();
         return response()->json(
@@ -53,12 +68,14 @@ class TaskController extends Controller
             204
         );
     }
-    public function isUpdated($id){
+    public function isUpdated($id)
+    {
         $task = Task::findOrFail($id);
         $task->title = 'Updated title';
-       $value1= $task->isDirty();
-       return response()->json(
-           $value1,200
-       );
+        $value1 = $task->isDirty();
+        return response()->json(
+            $value1,
+            200
+        );
     }
 }
